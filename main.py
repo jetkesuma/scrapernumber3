420789a7c85312412a32159d860d000cbcf8672e
Kusuma <kusumo999666@gmail.com>
2023-11-08 20:06:31 +0700

    req 403

---
+++ main.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://www.yell.com/s/builders-boston.html'
+
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36'}
+
+res = requests.get(url, headers=headers)
+print(res.status_code)
+
